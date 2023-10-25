c40b6535267a9aa1a987d1bb302afc4312c93130
olwpad <olwpad@gmail.com>
2023-10-25 07:46:41 -0500

    cambios

--- task-manager/src/App.jsx
+++ task-manager/src/App.jsx
@@ -1,52 +1,75 @@
-import { useState,useEffect
- } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import AddTaskForm from "./components/AddTaskForm";
 import Header from "./components/Header";
 import TaskList from "./components/TaskList";
-import axios from "axios"
+
 import "./styles/App.scss";
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
-
+  // useEffect se ejecuta una sola vez cuando se monta el componente
   useEffect(() => {
-  const getTasks=async()=>{
+    const getTasks = async () => {
+      try {
+        // Hace la petición al backend
+        const result = await axios.get(import.meta.env.VITE_BACKEND_URL);
 
-    try {
-      const result= await axios.get("http://127.0.0.1:3000/tasks");
-      if(result.status===200)
-      setTasks(result.data)
-    } catch (error) {
-      alert(error)
-    }
+        // Verifica el éxito de la petición
+        if (result.status === 200) {
+          // Modifica el estado
+          setTasks(result.data);
+        }
+      } catch (error) {
+        console.error("No hubo conexión al backend");
+      }
+    };
 
-  }
-  getTasks();
-  }, [])
-  
-  const onDeleteHandler = (id) => {
+    // Invoca la función para comunicarse con el backend
+    getTasks();
+  }, []);
+
+  const onDeleteHandler = async (id) => {
     if (confirm("Are you sure you want to delete the task?")) {
-      // Elimina el elemento filtrando el arreglo por el id de cada tarea
-      // No se puede modificar la variable tasks porque es INMUTABLE
-      const resultado = tasks.filter((tarea) => tarea.id !== id);
-      // Modifica el estado
-      setTasks(resultado);
+      try {
+        // Elimina la tarea del backend
+        const result = await axios.delete(
+          `${import.meta.env.VITE_BACKEND_URL}${id}`
+        );
+        if (result.status === 200) {
+          // Elimina el elemento filtrando el arreglo por el id de cada tarea
+          // No se puede modificar la variable tasks porque es INMUTABLE
+          const resultado = tasks.filter((tarea) => tarea.id !== id);
+          // Modifica el estado
+          setTasks(resultado);
+        }
+      } catch (error) {
+        alert("Error eliminando la tarea\nIntente más tarde");
+        console.log(error);
+      }
     }
   };
 
   const onCreateHandler = async (text) => {
-    // Crea un objeto para la nueva tarea
-    const newTask = {
-      text,
-    };
-    // Crea la tarea en el backend
-    const result = await axios.post("http://localhost:3000/tasks", newTask);
-    // Crea un nuevo arreglo basado en los elementos del arreglo tasks agregando la nueva al nueva tarea al final
-    const newTasks = [...tasks, result.data];
-    // Modifica el estado
-    setTasks(newTasks);
+    try {
+  
+      const newTask = {
+        text,
+      };
+      const result = await axios.post(
+        import.meta.env.VITE_BACKEND_URL,
+        newTask
+      );
+      if (result.status === 201) {
+        const newTasks = [...tasks, result.data];
+        setTasks(newTasks);
+      }
+    } catch (error) {
+      alert("Error creando la tarea\nIntente más tarde!");
+      console.log("Hubo un error de comunicación creando la tarea!");
+    }
   };
 
   return (
